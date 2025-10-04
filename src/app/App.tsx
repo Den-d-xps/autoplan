12ce417c3fc3f494843ba25762df174bbc837c21
Denis Kostyuhin <den.d.xps@gmail.com>
2025-10-04 16:16:18 +0700

    feat:add-redux-and-init-store

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,14 +5,18 @@ import CssBaseline from '@mui/material/CssBaseline';
 import AppTheme from '../features/theme/AppTheme';
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { Provider } from 'react-redux';
+import { store } from './providers/store/store';
 
 function App() {
   return (
     <AppTheme>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <CssBaseline enableColorScheme />
-        <RouterProvider router={router} />
-      </LocalizationProvider>
+      <Provider store={store}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <CssBaseline enableColorScheme />
+          <RouterProvider router={router} />
+        </LocalizationProvider>
+      </Provider>
     </ AppTheme>
   )
 }
---
+++ src/app/providers/store/store.ts
@@ -0,0 +1,21 @@
+import { combineSlices, configureStore } from '@reduxjs/toolkit';
+import {
+  type TypedUseSelectorHook,
+  useDispatch as dispatchHook,
+  useSelector as selectorHook
+} from 'react-redux';
+
+const rootReducer = combineSlices(
+  // здесь слайсы
+);
+
+export const store = configureStore({
+  reducer: rootReducer,
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch: () => AppDispatch = () => dispatchHook();
+export const useAppSelector: TypedUseSelectorHook<RootState> = selectorHook;
+
